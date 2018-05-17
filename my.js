4055ea93f7fc4bb62a6b5d9193c8b0106fbbf244
A64Danil <a64danil@mail.ru>
2018-05-17 23:06:36 +0300

    add QLoader to first exercise, add new City-list with 20 cities

--- my.js
+++ my.js
@@ -8,7 +8,7 @@ var cityList = document.body.querySelector('.cityList');
 var newCityList = []; // City List with hints
 var findCity = []; // Resulf of maching
 
-const textField =  document.createElement('input'); // City Input
+var textField =  document.createElement('input'); // City Input
 
 var textFieldHintsWrap =  document.createElement('div'); // Place for Hints
 var textFieldClear =  document.createElement('div'); // Clear value of text field
@@ -21,8 +21,31 @@ const cityRender = Handlebars.compile(cityTpl);
 const cityItemTpl = document.querySelector('#cityItem-tpl').textContent;
 const cityItemRender = Handlebars.compile(cityItemTpl);
 
-function loadTowns(url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json') {
-    console.log('1) пытаемся загрузить города, ' + url)
+function addElement(tag, className, textContent, attributes, place = document.body, before) {
+    var el = document.createElement(tag);
+    if (className != null) el.setAttribute('class', className);
+    if (textContent != null) el.textContent = textContent;
+
+    if (attributes != null) {
+        if (!(attributes instanceof Array)) {
+            console.warn('вы передаёте ' + attributes + ', a нужно передавать массив объектов типа [{ attrName1 : attrValue}, { attrName2 : attrValue}]');
+        }
+        else if (attributes.length > 0) {
+            console.log();
+            for (var i = 0; i < attributes.length; i++ ) {
+                let currentKey = Object.keys(attributes[i]);
+                console.log('были переданы параметры, ' + attributes.length + ' шт.; Name: ' + currentKey[0] + ', ' +
+                    'value: ' + attributes[i][currentKey[0]]); // name + value
+                el.setAttribute(currentKey[0], attributes[i][currentKey[0]]);
+            }
+
+        }
+    }
+    before ? document.body.insertBefore(el, place) :  place.appendChild(el);
+}
+
+function loadTowns(url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities2.json') {
+    console.log('1) Пытаемся загрузить города отсюда: ' + url)
     return new Promise ((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.open('GET', url);
@@ -30,7 +53,7 @@ function loadTowns(url = 'https://raw.githubusercontent.com/smelukov/citiesTest/
         xhr.addEventListener('load', () => {
 
             if (xhr.status !== 200) {
-                console.log('1.2)всё пошло по пизде');
+                console.log('1.2) Что-то пошло не так!');
                 reject(xhr.status);
             }
 
@@ -49,7 +72,7 @@ function loadTowns(url = 'https://raw.githubusercontent.com/smelukov/citiesTest/
                 return 0;
             });
 
-            console.log('2) после сортировки');
+            console.log('2) После сортировки городов');
             console.log(result);
             resolve(result);
         });
@@ -58,17 +81,15 @@ function loadTowns(url = 'https://raw.githubusercontent.com/smelukov/citiesTest/
 }
 
 function createTextField() {
+    console.log('3.1) внутри createTextField')
     var content = document.body.querySelector('.content');
-    //var cityList = document.body.querySelector('.cityList');
     var textFieldLabel = document.createElement('label');
     textFieldLabel.textContent = "Поиск по списку городов";
-    //cityList.appendChild(textFieldLabel);
+
     cityList.insertBefore(textFieldLabel, content);
     var textFieldWrap =  document.createElement('div'); // City Input Wrapper
     textFieldWrap.setAttribute('class', 'input-group');
     cityList.insertBefore(textFieldWrap, content);
-    //cityList.appendChild(textFieldWrap);
-    //console.warn(textFieldWrap);
 
 
     textField.setAttribute('type', 'text');
@@ -82,13 +103,13 @@ function createTextField() {
     textFieldWrap.appendChild(resCountWrap);
     textFieldWrap.appendChild(textFieldClear);
 
-
     textFieldClear.addEventListener('click', function (e) {
         textField.value = '';
         resCountWrap.textContent = null;
         findPartial(newCityList, textField.value);
     })
 
+    console.log('3.2) Перед добавлением события keyup')
     textField.addEventListener('keyup', findOnPress);
 
 }
@@ -99,7 +120,6 @@ function cityListCreate(value) {
         cityList: value
     };
     var cityHtml = cityRender(tempVal);
-    //console.log(cityHtml);
     cityList.innerHTML += cityHtml;
 
     console.log(value);
@@ -113,7 +133,6 @@ function cityListCreate(value) {
 }
 
 function iconRadomizer() {
-    console.log('Твои города:');
     var allCitys = document.querySelectorAll('.cityIco')
 
     allCitys.forEach(function(item, i, arr) {
@@ -121,7 +140,6 @@ function iconRadomizer() {
         let top = 65 * Math.floor(getRandomArbitary(0, -5));
         item.style.backgroundPosition = `${right}px ${top}px`;
     });
-    //console.log(allCitys);
 }
 
 function getRandomArbitary(min, max) {
@@ -129,30 +147,58 @@ function getRandomArbitary(min, max) {
 }
 
 function tryAgain() {
-    console.log('Запустили трай эгэйн')
-    var againBut =  document.createElement('button');
-    againBut.textContent = 'Попробовать загрузить снова';
+    console.log('Запустили трай эгэйн');
 
-    cityList.innerHTML += '<p class="error_msg">Не удалось загрузить список</p>'
-    cityList.appendChild(againBut);
+    cityList.innerHTML += '<p class="error_msg">Не удалось загрузить список!</p>';
+    cityList.innerHTML += '<p class="try_msg">Попробовать загрузить снова <span class="newUrl"></span></p>';
+    addElement('select', 'listOfLoads', 'Варианты загрузки', null, cityList);
+    var listOfLoads =  document.querySelector('.listOfLoads');
+
+    addElement('option', null, 'Список с битой ссылкой', [
+        { 'value' : 'http://yandex.ru' }
+    ], listOfLoads);
+    addElement('option', null, 'Классический список из 50 городов', [
+        { 'value' : 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json' }
+    ], listOfLoads);
+    addElement('option', null, 'Новый список из 20 городов', [
+        { 'value' : 'http://pro.qoobeo.ru/acf-tpl/cities20.json' }
+        ], listOfLoads);
+    addElement('option', 'shortList', 'Новый короткий список из 10 городов', [
+        { 'value' : 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities3.json' }]
+        , listOfLoads);
+
+    addElement('button', 'againBut', 'Попробовать загрузить снова', [{value : 'http://yandex.ru'}], cityList)
+
+    var againBut = document.querySelector('.againBut')
 
     againBut.addEventListener('click', function () {
-        console.log('click');
-        let url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json';
+        showLoader('showAgain');
+        console.log('click, ' + againBut.value);
+        let url = againBut.value;
         loadTowns(url)
             .then((value) => {
                 console.log('Повторная загрузка успешно завершена');
-                cityListCreate(value)
-                //cityListCreate(cityHtml);
-                var message = document.querySelector('.error_msg');
-                //document.removeChild(message);
-                //document.removeChild(againBut);
-                //console.log('Лишние элементы удалены');
+                cityListCreate(value);
+                var message = document.body.querySelector('.error_msg');
+                var againBut = document.body.getElementsByTagName('button');
+                cityList.removeChild(message);
+                cityList.removeChild(againBut[0]);
+                cityList.removeChild(document.body.getElementsByTagName('select')[0]);
+                cityList.removeChild(document.body.querySelector('.try_msg'));
+                console.log('Лишние элементы удалены');
+                console.log('3) Перед создание поля (внутри tryAgain)');
+                createTextField();
                 },
                 (value) => {
                 console.log('Повторная загрузка, код ошибки: ' + value);
-            }
-        );
+                }
+            )
+            .then(
+                () => {
+                    console.log('4) Then в конце TryAgain');
+                    removeLoader();
+                }
+            );
     })
 
 }
@@ -164,13 +210,11 @@ document.body.addEventListener('click', function () {
 
 document.body.addEventListener('click', function (e) {
     if (e.target.tagName === 'LI') {
-        console.log('повезло, вы кликнули конкретно на пункт');
         block = e.target;
         textField.value = block.dataset.name;
         findOnPress();
     }
     else  if (e.target.parentNode.tagName === 'LI') {
-        console.log('не сам Ли, но всеравно');
         block = e.target.parentNode;
         textField.value = block.dataset.name;
         findOnPress();
@@ -179,6 +223,13 @@ document.body.addEventListener('click', function (e) {
 
     //textField.addEventListener('keyup', findOnPress);
 
+}, false)
+
+document.body.addEventListener('click', function (e) {
+    if (e.target.tagName === 'SELECT') {
+        document.querySelector('.againBut').value = e.target.selectedOptions[0].value;
+        document.querySelector('.newUrl').textContent = "(new URL: " + e.target.selectedOptions[0].value + ")";
+    }
 }, false)
 
 function findPartial( arr, ell ) {
@@ -197,12 +248,9 @@ function findPartial( arr, ell ) {
     for (let i = 0; i < arr.length; i++) { // Заполняем подсказками
         if (arr[i].match(regex) ) {
             let curItem = { name : arr[i] }
-
-            //TODO: обернуть весь массив внутрь объекта cityItemList
             tepmArr.push(curItem);
-
             resCount++;
-            console.log("Точное совпадение, " + arr[i])
+            //console.log("Точное совпадение, " + arr[i])
         }
     }
     var tempVal = {
@@ -221,25 +269,28 @@ function findOnPress() {
     findPartial( newCityList, textField.value);
 }
 
-function pageLoader() {
-    return new Promise ((resolve) => {
-        var loadWrap =  document.body.querySelector('.loader-wrap');
-        window.addEventListener('load', function () {
-           document.body.removeChild(loadWrap);
-           loadText = null;
-           loadWrap = null;
-           resolve();
-       });
-    });
-}
+// TODO: сделать правильную загрузку лоадера
+// function pageLoader() {
+//     return new Promise ((resolve) => {
+//         var loadWrap =  document.body.querySelector('.loader-wrap');
+//         window.addEventListener('load', function () {
+//            document.body.removeChild(loadWrap);
+//            loadText = null;
+//            loadWrap = null;
+//            resolve();
+//        });
+//     });
+// }
 
-pageLoader();
+// pageLoader();
 
 loadTowns() // после отправки запроса
     .then(
         (value) => {
             console.log('Успешно отгрузили результат');
             cityListCreate(value);
+            console.log('3) Перед создание поля');
+            createTextField();
         },
         (value) => {
             tryAgain();
@@ -248,8 +299,7 @@ loadTowns() // после отправки запроса
     )
     .then(
         () => {
-            console.log('3) Перед создание поля');
-            createTextField();
+            console.log('4) Then в конце');
         }
     )
 
